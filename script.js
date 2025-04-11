0c1f9643266f697abdea3fadaf210858e36fd426
HarshVardhanLab <harshvardhan02102002@gmail.com>
2025-04-11 10:14:52 +0530

    Time left for registration

---
+++ script.js
@@ -0,0 +1,25 @@
+const countDownDate = new Date("April 26, 2025 23:59:59").getTime();
+const countdown = setInterval(function() {
+
+  const now = new Date().getTime();
+
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+
+  document.getElementById("days").textContent = days;
+  document.getElementById("hours").textContent = hours;
+  document.getElementById("minutes").textContent = minutes;
+  document.getElementById("seconds").textContent = seconds;
+  
+
+  if (distance < 0) {
+    clearInterval(countdown);
+    document.querySelector(".countdown-timer").innerHTML = 
+      "<div class='expired-message'>Registration Closed</div>";
+  }
+}, 1000);
